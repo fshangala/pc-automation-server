12907323ebb722c7820a311ba821c0d1a94600c3
fshangala <fshangala@gmail.com>
2022-12-23 15:11:47 +0530

    removed db access from asgi

--- automations/automations/asgi.py
+++ automations/automations/asgi.py
@@ -8,21 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-import django
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automations.settings')
-django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import pcautomation.routing
-from pcautomation.models import Connection
-
-
-for item in Connection.objects.all():
-    item.delete()
-
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
