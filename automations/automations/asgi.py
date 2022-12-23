71f4eaccf8a0677f9df7f22e12a88804d8ae805e
fshangala <fshangala@gmail.com>
2022-12-23 14:34:49 +0530

    updated asgi

--- automations/automations/asgi.py
+++ automations/automations/asgi.py
@@ -9,13 +9,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automations.settings')
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import pcautomation.routing
 from pcautomation.models import Connection
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automations.settings')
 
 for item in Connection.objects.all():
     item.delete()
