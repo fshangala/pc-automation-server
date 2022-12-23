b7df8848aaf605797b0c54ebdf107d092c3987b0
fshangala <fshangala@gmail.com>
2022-12-23 19:41:20 +0530

    working in localhost with daphne

--- automations/automations/asgi.py
+++ automations/automations/asgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
-import os
+import os, django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'automations.settings')
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
