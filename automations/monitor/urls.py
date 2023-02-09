cf12d763b964a5a745402f0faa955216bb707e03
fshangala <fshangala@gmail.com>
2023-02-09 17:38:00 +0200

    created testing page

--- automations/monitor/urls.py
+++ automations/monitor/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from .views import (
+  MonitorView,
+  TestingView
+)
 
 app_name="monitor"
-urlpatterns=[]
+urlpatterns=[
+  path('',MonitorView.as_view(),name="index"),
+  path('testing/',TestingView.as_view(),name="testing")
+]
--- automations/monitor/views.py
+++ automations/monitor/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class MonitorView(TemplateView):
+  template_name="monitor/index.html"
+
+class TestingView(TemplateView):
+  template_name="monitor/testing.html"
