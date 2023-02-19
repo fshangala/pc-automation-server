c107d5a48443cad9c6ab9027a739ea006ab2adb1
fshangala <fshangala@gmail.com>
2023-02-19 08:33:57 +0200

    betsite managed by admin

--- automations/softwares/serializers.py
+++ automations/softwares/serializers.py
@@ -1,8 +1,13 @@
 from dataclasses import field, fields
 from rest_framework import serializers
-from softwares.models import Software
+from softwares.models import Software, BetSite
 
 class SoftwareSerializer(serializers.ModelSerializer):
     class Meta:
         model=Software
+        fields="__all__"
+
+class BetSiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=BetSite
         fields="__all__"
--- automations/softwares/views.py
+++ automations/softwares/views.py
@@ -1,11 +1,13 @@
 import mimetypes
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
-from .models import Software
+from .models import Software, BetSite
+from .serializers import BetSiteSerializer
 from django.contrib.auth.decorators import login_required
 import os
 from softwares.serializers import SoftwareSerializer
 from django.views.generic import ListView, DetailView
+from rest_framework import viewsets
 
 class SoftwareListView(ListView):
     model=Software
@@ -21,3 +23,7 @@ class SoftwareDetailView(DetailView):
         context["software"] = SoftwareSerializer(context["software"]).data
         
         return context
+
+class BetSiteViewset(viewsets.ReadOnlyModelViewSet):
+    queryset=BetSite.objects.all()
+    serializer_class=BetSiteSerializer
