357b6d3642b4b0287cff372f5f12ba7ec20b4edf
fshangala <fshangala@gmail.com>
2023-08-04 07:43:57 +0200

    profile is created

--- automations/accounts/tests.py
+++ automations/accounts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from accounts.models import Profile
+from django.contrib.auth.models import User
+from accounts.serializers import UserSerializer
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+  def test_profile_created(self):
+    user = User.objects.create(username="test",password="test")
+    user_json = UserSerializer(user).data
+    print(user_json)
